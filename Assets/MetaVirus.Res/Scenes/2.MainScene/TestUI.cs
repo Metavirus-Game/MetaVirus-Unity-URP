a5baf8f9cd264f46f7b0a0a08a08fd0cd02e2f28
tturaki <1091539@qq.com>
2023-02-06 21:37:00 -0800

    add res

---
+++ Assets/MetaVirus.Res/Scenes/2.MainScene/TestUI.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using FairyGUI;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class TestUI : MonoBehaviour
+{
+    GComponent _mainView;
+
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+    }
+
+    async void Start()
+    {
+        // _mainView = GetComponent<UIPanel>().ui;
+        // _mainView.GetChild("btnMain").onClick.Add(() => { Debug.Log("按了按了按了啊！"); });
+        //
+        // var go = await Addressables.LoadAssetAsync<GameObject>(
+        //     "Assets/MetaVirus.Res/Scenes/2.MainScene/Res/Particle_Tonado.prefab").Task;
+        // go = Instantiate(go);
+        // go.transform.localScale = new Vector3(70, 70, 70);
+        // _mainView.GetChild("effect").asGraph.SetNativeObject(new GoWrapper(go));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
