737f82a86316e48ca9ba64e1645fbfd576eca543
GaryJC <garyjiacheng@gmail.com>
2023-02-09 22:37:11 -0800

    arenaUI_draft_3

--- Assets/MetaVirus.Logic/UI/Component/ArenaList/ArenaMatchingListItem.cs
+++ Assets/MetaVirus.Logic/UI/Component/ArenaList/ArenaMatchingListItem.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using FairyGUI;
+using GameEngine;
+using MetaVirus.Logic.Service.Arena;
 using MetaVirus.Logic.Service.Arena.data;
+using MetaVirus.Logic.Service.UI;
+using MetaVirus.Logic.UI.Windows;
 using UnityEngine;
 
 namespace MetaVirus.Logic.UI.Component.ArenaList
@@ -11,19 +15,27 @@ namespace MetaVirus.Logic.UI.Component.ArenaList
         private GTextField _textEnemyName;
         public GComponent _matchingListItem;
         private GButton _buttonChallenge;
-
+        private UIService _uiService;
+        //private ArenaService _arenaService;
         public ArenaMatchingListItem()
         {
             _matchingListItem = UIPackage.CreateObject("Common", "MatchingListFrame").asCom;
             _textRanking = _matchingListItem.GetChild("text_ranking").asTextField;
             _textEnemyName = _matchingListItem.GetChild("text_enemyName").asTextField;
             _buttonChallenge = _matchingListItem.GetChild("button_challenge").asButton;
+            //_arenaService = GameFramework.GetService<ArenaService>();
+            _uiService = GameFramework.GetService<UIService>();
         }
 
         public void RenderMatchingListItem(ArenaPlayerData matchingData)
         {
             _textRanking.text = Convert.ToString(matchingData.ArenaInfo.Rank);
             _textEnemyName.text = matchingData.PlayerName;
+            // var task = _arenaService
+            _buttonChallenge.onClick.Set(() =>
+            {
+                _uiService.OpenWindow<UIPreparartion>();
+            });
         }
     }
 }
---
+++ Assets/MetaVirus.Logic/UI/Windows/UIPreparation.cs
@@ -0,0 +1,20 @@
+﻿using FairyGUI;
+using MetaVirus.Logic.Service.UI;
+using UnityEngine;
+
+namespace MetaVirus.Logic.UI.Windows
+{
+    public class UIPreparartion : BaseUIWindow
+    {
+        protected override GComponent MakeContent()
+        {
+            var comp = UIPackage.CreateObject("Common", "ArenaPreparationUI").asCom;
+            return comp;
+        }
+
+        public override void LoadData(GComponent parentComp, GComponent content)
+        {
+            
+        }
+    }
+}
